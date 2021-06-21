fdb176cfe43e9a82a35c19c05e74640d8424eab0
Pzacarias <pzacarias.4@gmail.com>
2021-06-21 11:43:41 -0300

    Se completa ClienteService y se crea PrestamoService. Se completan los ambos repos.

--- src/main/java/ar/com/ada/api/creditos/repos/ClienteRepository.java
+++ src/main/java/ar/com/ada/api/creditos/repos/ClienteRepository.java
@@ -1,11 +1,26 @@
 package ar.com.ada.api.creditos.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import ar.com.ada.api.creditos.entities.Cliente;
+import java.util.*;
+import ar.com.ada.api.creditos.entities.*;
 
 @Repository
-public interface ClienteRepository extends JpaRepository <Cliente, Integer> {
-    
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    Cliente findByNombre(String nombreCliente);
+
+    Cliente findByDni(Integer dni);
+
+    List<Cliente> findAllByNombreContiene(String nombre);
+
+    List<Cliente> findAllByNombreAndDNI(String nombre, Integer dni);
+
+    @Query("select h from Cliente h order by nombre")
+    List<Cliente> findAllOrderByNombre();
+
+    @Query("SELECT h FROM Cliente h WHERE h.dni = :dni")
+    List<Cliente> findByDNIVersion2(@Param("dni") Integer descripcion);
 }
--- src/main/java/ar/com/ada/api/creditos/repos/PrestamoRepository.java
+++ src/main/java/ar/com/ada/api/creditos/repos/PrestamoRepository.java
@@ -1,8 +1,16 @@
 package ar.com.ada.api.creditos.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import ar.com.ada.api.creditos.entities.*;
 
 public interface PrestamoRepository extends JpaRepository<Prestamo, Integer>{
-    
+
+    @Query("FROM Prestamo WHERE cliente.nombre like CONCAT('%', :nombre,'%')")
+    List<Prestamo> findByNombreCliente(@Param("nombre") String nombre);
+
 }
